d2289fa6ae932a4eee39dc7b3e17b388ee48343d
vinceecws <vinceecws@gmail.com>
2020-10-20 14:04:33 -0400

    minor fix

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import Player from './components/Player.js'
 import SessionSideList from './components/Sessions/SessionsSideList.js';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
@@ -7,6 +8,7 @@ function App() {
   
   return (
     <div>
+      <Player/>
       <header className='Session-Side-List'>
         <SessionSideList/>
       </header>
---
+++ client/src/components/Player.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+/*
+var formData  = new FormData();
+
+formData.append("format", "json");
+formData.append("url", "http://soundcloud.com/forss/flickermood");
+
+var html = fetch('http://soundcloud.com/oembed', {
+    method: 'POST',
+    body: formData
+}).then(function (response) {
+    return response.json();
+}).then(function (res) {
+  return res.html;
+});
+*/
+
+class Player extends React.Component{
+    render(){
+        return(
+            null
+        )
+    }
+}
+
+export default Player;
