4bbd3b122abd8425a6eb718bfc52c754e0ee9a9f
Vincent Chooi <vincey@Vincents-MacBook-Pro.local>
2020-11-09 17:07:19 -0500

    Moved google oauth2 clientid and clientsecret to server for security purposes

---
+++ server/src/const/index.js
@@ -0,0 +1,2 @@
+export const google_oauth2_client_id = '1062386445238-hpt4garm04t6lfcn82brmbic5gng7u8i.apps.googleusercontent.com'
+export const google_oauth2_client_secret = 'tCbLxNT116LjzTJdALkzvDfb'
