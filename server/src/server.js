394618094eed5ef38f5bfb844594c2159119eb22
Velite12 <schamorro357@gmail.com>
2020-11-09 20:06:44 -0500

    testing axios front to back

--- server/src/server.js
+++ server/src/server.js
@@ -1,13 +1,17 @@
 const passport = require("passport")
-
+const mongoose = require('mongoose')
 const GoogleStrategy = require("passport-google-oauth20").Strategy
 const LocalStrategy = require('passport-local').Strategy
 
 const bodyParser = require('body-parser')
 const cors = require('cors')
+const express = require("express")
 const app = express()
 const db = require('./db')
+const userSchema = require('./db/Schema/userSchema.js')
 const apiPort = 4000
+
+const User = mongoose.model('user', userSchema, 'user')
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cors())
@@ -30,8 +34,20 @@ passport.use(new LocalStrategy(function(username, password, done) {
      });
 }));
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+const HarmonyRoutes = express.Router();
+
+app.use('/main', HarmonyRoutes);
+
+HarmonyRoutes.route('/home').get(function(req, res) {
+	User.find(function(err, users){
+		if (err){
+			console.log(err);
+		} else {
+			
+			res.send("Obtained Users");
+		}
+	});
+});
+
 
 app.listen(apiPort, () => console.log(`Server running on port ${apiPort}`))
