8bb5d0253ff269678d0d5775ed3a7df70eb88042
Velite12 <schamorro357@gmail.com>
2020-11-12 11:25:49 -0500

    fixed typo

--- server/src/db/Schema/collectionSchema.js
+++ server/src/db/Schema/collectionSchema.js
@@ -9,7 +9,7 @@ const collectionSchema = new mongoose.Schema ({
     ownerId:{
         type:String,
         default: ""
-    }
+    },
     description: {
         type: String,
         default: ""
