6cf609404c3122edd8cef7ed2b8b69080aab1697
Velite12 <schamorro357@gmail.com>
2020-12-02 02:42:12 -0500

    merge change heroku

---
+++ client/build/static/js/0_index.0f79f22a.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpharmony=this.webpackJsonpharmony||[]).push([[0],{370:function(n,h,o){}}]);
+//# sourceMappingURL=0_index.0f79f22a.chunk.js.map
---
+++ client/build/static/js/6.5f193ef2.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpharmony=this.webpackJsonpharmony||[]).push([[6],{373:function(n,h,o){}}]);
+//# sourceMappingURL=6.5f193ef2.chunk.js.map
