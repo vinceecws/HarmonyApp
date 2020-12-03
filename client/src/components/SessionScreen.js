87d4b2bf82825da2521aefdeeaebde4f5c27416d
Velite12 <schamorro357@gmail.com>
2020-12-03 13:23:25 -0500

    Update SessionScreen.js

--- client/src/components/SessionScreen.js
+++ client/src/components/SessionScreen.js
@@ -402,12 +402,8 @@ class SessionScreen extends React.Component {
 							name: session.name,
 							startTime: session.startTime,
 					});
-<<<<<<< HEAD
-					this.initSessionClient(session._id);
-=======
 				this.initSessionClient(session._id);
 					
->>>>>>> d48fb8ecc4f3488efc7bb36ce34b9f4dc8bf063e
 			}
             
         }
